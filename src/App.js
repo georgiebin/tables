22c06c10c80705c83ffe7687ee2f9883b3f0e349
georgiebin <nuclearbloom@gmail.com>
2019-04-08 19:58:14 +0200

    introduce css grid

--- src/App.js
+++ src/App.js
@@ -10,14 +10,27 @@ class App extends Component {
       { id: 'beta', version: '1.36.3', assignment: [1524]},  
       { id: 'c1-protected', version: '1.36.0', assignment: [2312314,11123124]},  
       { id: 'georgie-test', version: 'branches/PLAY-1552', assignment: [23151324]},  
-    ]
+    ],
   };
 
   deleteRowHandler = (rowIndex) => {
     const rows = [...this.state.rows];
-    rows.splice(rowIndex, 1);
+    const deletedRow = rows.splice(rowIndex, 1);
+    console.log(deletedRow);
     this.setState({rows: rows});
   };
+
+  editRowHandler = (rowIndex) => {
+    console.log('clicked on ' + rowIndex);
+  };
+
+  /*
+  * the hovered row should show the delete link
+  */
+  setRowHovered = (isHovered, rowIndex) => {
+    const eventText = isHovered ? 'is hovered' : 'not hovered'
+    console.log(rowIndex + ' ' + eventText);
+  }
 
   render() {
     return (
@@ -25,7 +38,9 @@ class App extends Component {
         <header className="App-header">
           {this.state.rows.map((row, index) => {
             return <Row 
-              click={() => this.deleteRowHandler(index)}
+              click={() => this.editRowHandler(index)}
+              hover={() => this.setRowHovered(true, index)}
+              leave={() => this.setRowHovered(false, index)}
               name={row.id} 
               version={row.version} 
               scope={row.assignment} 
--- src/Table/Row.js
+++ src/Table/Row.js
@@ -4,8 +4,9 @@ import './Table.css';
 
 const Row = ( props ) => {
     return (
-        <div className="row" onClick={props.click}>
+        <div className="row" onClick={props.click} onMouseEnter={props.hover} onMouseLeave={props.leave}>
             <div>{props.name}</div>
+            <div></div>
             <div>{props.version}</div>
             <div>{props.scope.join(', ')}</div>
         </div>
